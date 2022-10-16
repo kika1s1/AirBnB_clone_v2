a80d45630da127f55311755addabb5d104348678
root <root@DESKTOP-V0N0OBS.localdomain>
2022-10-16 20:00:00 +0300

    new

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,41 +1,55 @@
 #!/usr/bin/python3
-"""Flask framework
-"""
+""" script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes: 1- /: display “Hello HBNB!”
+            2- /hbnb: display “HBNB”
+            3- /c/<text>: display “C ” followed by the value of the text
+                variable (replace underscore _ symbols with a space )
+            4- /python/(<text>): display “Python ”, followed by the value of...
+                the text variable (replace underscore _ symbols with a space )
+                The default value of text is “is cool”
+            5- /number/<n>: display “n is a number” only if n is an integer
+    You must use the option strict_slashes=False in your route definition """
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """return hello hbhb
-    """
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ hello_hbnb method """
+    return ('Hello HBNB!')
 
 
-@app.route("/hbnb", strict_slashes=False)
-def HBNB():
-    """return HBNB"""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def only_hbnb():
+    """ only_hbnb method: """
+    return ('HBNB')
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text(text):
-    """return text given"""
-    return "C {}".format(text.replace("_", " "))
+def only_c(text):
+    """ only_c method: route to return C followed by text variable, replaces _
+        with spaces """
+    text = text.replace('_', ' ')
+    return ('C' + ' ' + text)
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def display(text):
-    """display “Python ”, followed by the value of the text"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<path:text>', strict_slashes=False)
+def only_python(text=None):
+    """ only_python method: route to return text follow by "is cool"
+        (can be overwritten), replaces _ with spaces """
+    if text is None:
+        text = 'is cool'
+    else:
+        text = text.replace('_', ' ')
+    return ('Python' + ' ' + text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num_display(n):
-    """display “n is a number” only"""
-    return "{} is a number".format(n)
+def number_int(n):
+    """  number_int method: display “n is a number” only if n is an integer """
+    return ('{:d} is a number'.format(n))
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+""" this module contains a script that starts a Flask web application
+    the web application must be listening on 0.0.0.0, port 5000
+    Routes: - /states_list """
+from models import *
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from flask import Flask, render_template
-import models
+app = Flask(__name__)
 
-app = Flask("__name__")
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id=None):
+    """ display HTML page with list of states """
+    all_states = storage.all(State)
+    if id:
+        states = all_states.get('State.{}'.format(id))
+    else:
+        states = all_states.values()
+    # ^ fetches states data from storage engine, then in line below,
+    # those states are passed into the template
+    return render_template('9-states.html', states=states)
 
 
 @app.teardown_appcontext
-def refresh(exception):
-        models.storage.close()
+def remove_SQLalc_session(exception):
+    """ close storage when tear down is called """
+    storage.close()
 
-
-@app.route("/states", strict_slashes=False)
-def route_states():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('7-states_list.html', states_list=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def route_city():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('8-cities_by_states.html', states_list=states)
-
-
-if __name__ == "__main__":
-        app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
